bded67700dfec809b40f7345e938c1f8fda5da27
DeyaRose <56009202+DeyaRose@users.noreply.github.com>
2021-05-08 22:02:31 -0500

    Updated setup file and added installation functionality

--- setup.sh
+++ setup.sh
@@ -3,39 +3,71 @@
 # A setup script to link the config files to the places they need to be.
 # Assumes files have been cloned into ~/Config/
 
+# Ask the user if they want to install any of the programs not already installed
 
+tmuxi="$(which tmux)"
+if [ -z $tmuxi ]; then
+    echo "Tmux not installed..."
+    tmuxi="tmux"
+else
+    tmuxi=""
+fi
+
+vimi="$(which vim)"
+if [ -z $vimi ] && [ "${vimi: -4}" != "nvim" ]; then
+    echo "Vim not installed..."
+    vimi="vim"
+else
+    vimi=""
+fi
+
+if [ ! -z "$tmuxi" ] || [ ! -z "$vimi" ]; then
+    echo -n "Install missing programs? (y/n) > "
+    read user_i
+    # FIXME: might not always have apt or apt-get
+    if [[ $user_i = "y" || $user_i = "ye" || $user_i = "yes" ]]; then
+        /bin/bash -c "$(which sudo) apt-get install $tmuxi $vimi"
+    else
+        echo "Not installing."
+    fi
+fi
+
+# Install the config files to their respective places
+## .tmux.conf
 if [ ! -f "~/.tmux.conf" ]; then
     ln -s ~/Config/tmux.conf ~/.tmux.conf
     code=$?
-    if [ -z $code ]; then
+    if [ $code = 0 ]; then
         echo "Tmux config successfully linked."
     else
         echo "Error in linking tmux config: $code"
     fi
 else
-    echo "Tmux config already linked."
+    echo "Tmux config already exists."
 fi
 
+## .vimrc
 if [ ! -f "~/.vimrc" ]; then
     ln -s ~/Config/vimrc ~/.vimrc
     code=$?
-    if [ -z $code ]; then
+    if [ $code = 0 ]; then
         echo "Vim config successfully linked."
     else
         echo "Error in linking vim config: $code"
     fi
 else
-    echo "Vim config already linked."
+    echo "Vim config already exists."
 fi
 
+## .bash_aliases
 if [ ! -f "~/.bash_aliases" ]; then
     ln -s ~/Config/bash_aliases ~/.bash_aliases
     code=$?
-    if [ -z $code ]; then
+    if [ $code = 0 ]; then
         echo "Bash aliases successfully linked."
     else
         echo "Error in linking Bash aliases: $code"
     fi
 else
-    echo "Bash aliases already linked."
+    echo "Bash aliases already exists."
 fi
