315dece0a26313ef00e194ae4a9f6a428da1f87e
seborbital <56009202+seborbital@users.noreply.github.com>
2021-08-02 21:33:50 -0500

    Fixed temperature.sh for some systems using lm-sensors

--- temperature.sh
+++ temperature.sh
@@ -4,7 +4,12 @@ if [ -f "/opt/vc/bin/vcgencmd" ]; then
     /opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print($2 * 1.8)+32}'
 elif [ ! -z $(which sensors) ]; then
     # package lm-sensors
-    sensors | grep "CPU temp" | awk -F "[:°+]" '{print($3 * 1.8)+32}'
+    #sensors | grep "CPU temp" | awk -F "[:°+]" '{print($3 * 1.8)+32}'
+    sensors |\
+    # assume that the first instance is the cpu temperature
+    # (it may not be for every device but it is on the ones i use)
+    grep "id 0:" |\
+    awk -F "[:°+]" '{print($3 * 1.8)+32}'
 else
     # TODO: figure something else out later
     echo -n "~"
