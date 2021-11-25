a36c74cc8b149464ae3cbce7438d463b81ed8512
seborbital <56009202+seborbital@users.noreply.github.com>
2021-11-24 23:29:02 -0600

    Updated temperature.sh to fix for raspberry pi

--- temperature.sh
+++ temperature.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+thermo="\xf0\x9f\x8c\xa1\xb8\x8f" # the thermometer emoji
 if [ -f "/opt/vc/bin/vcgencmd" ]; then
-    /opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print($2 * 1.8)+32}'
+    # measuring temperature on raspberry pi
+    temp=$(/opt/vc/bin/vcgencmd measure_temp |\
+        awk -F "[=\']" '{print($2 * 1.8)+32}')
+    if [ ! -z $temp ]; then
+        echo -en "$thermo $temp°F"
+    fi
+    echo -n ' ' # space for padding
 elif [ ! -z $(which sensors) ]; then
     # package lm-sensors
     #sensors | grep "CPU temp" | awk -F "[:°+]" '{print($3 * 1.8)+32}'
@@ -10,12 +17,8 @@ elif [ ! -z $(which sensors) ]; then
     # (it may not be for every device but it is on the ones i use)
     grep "id 0:" |\
     awk -F "[:°+]" '{print($3 * 1.8)+32}')
-    thermo="\xf0\x9f\x8c\xa1\xb8\x8f" # the thermometer emoji
-    echo -en "$thermo "
     if [ ! -z $temp ]; then
-        echo -n "$temp°F"
-    else
-        echo -n "Err"
+        echo -en "$thermo $temp°F"
     fi
     # 🌡( #(/bin/bash ~/Config/temperature.sh)°F
     echo -n ' ' # space for padding
