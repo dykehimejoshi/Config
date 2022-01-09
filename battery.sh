70cff26f28c4e7cc7dc553241055f10760180cd6
seborbital <56009202+seborbital@users.noreply.github.com>
2022-01-09 00:22:04 -0600

    Updated tmux.conf

--- battery.sh
+++ battery.sh
@@ -3,8 +3,6 @@
 if [ ! -z $(which upower) ]; then
     # if upower is installed
     charge_state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{$1="state:"; print $2}')
-    heart="\xe2\x99\xa5\xef\xb8\x8f" # heart emoji (the suit because i think it looks better)
-    echo -en "$heart "
     if [[ $charge_state == 'discharging' ]]; then
         echo -n "-"
     elif [[ $charge_state == 'charging' ]]; then
@@ -13,7 +11,6 @@ if [ ! -z $(which upower) ]; then
         echo -n ""
     fi
     echo -n $(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{$1="percentage:"; print $2}')
-    echo -n ' ' # space for padding
 else
     echo -n ""
 fi
--- temperature.sh
+++ temperature.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-thermo="\xf0\x9f\x8c\xa1\xb8\x8f" # the thermometer emoji
 if [ -f "/opt/vc/bin/vcgencmd" ]; then
     # measuring temperature on raspberry pi
     temp=$(/opt/vc/bin/vcgencmd measure_temp |\
         awk -F "[=\']" '{print($2 * 1.8)+32}')
     if [ ! -z $temp ]; then
-        echo -en "$thermo $temp°F"
+        echo -en "$temp°F"
     fi
-    echo -n ' ' # space for padding
+    echo -n ' | ' # space for padding
 elif [ ! -z $(which sensors) ]; then
     # package lm-sensors
     #sensors | grep "CPU temp" | awk -F "[:°+]" '{print($3 * 1.8)+32}'
@@ -18,10 +17,10 @@ elif [ ! -z $(which sensors) ]; then
     grep "id 0:" |\
     awk -F "[:°+]" '{print($3 * 1.8)+32}')
     if [ ! -z $temp ]; then
-        echo -en "$thermo $temp°F"
+        echo -en "$temp°F"
     fi
+    echo -n ' | '
     # 🌡( #(/bin/bash ~/Config/temperature.sh)°F
-    echo -n ' ' # space for padding
 else
     # TODO: figure something else out later
     echo -n ""
