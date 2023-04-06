923b3d3ce74a74864c6802bebc60930640e23f9a
seborbital <56009202+seborbital@users.noreply.github.com>
2023-04-06 11:16:15 -0500

    Minor refactoring in battery.sh

--- battery.sh
+++ battery.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ ! -z $(command -v upower) ]; then
-    charge_state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{$1="state:"; print $2}')
-    percentage=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{$1="percentage:"; print $2}')
+    cmd_output=$(upower -i $(upower -e | grep BAT))
+    charge_state=$(echo "$cmd_output" | grep --color=never -E "state" | awk '{$1="state:"; print $2}')
+    percentage=$(echo "$cmd_output" | grep --color=never -E "percentage" | awk '{$1="percentage:"; print $2}')
 elif [ ! -z $(command -v termux-battery-status) ]; then
-    # this takes more than 0.2 seconds to run, so let's do it once and work from there
     cmd_output=$(termux-battery-status)
     charge_state=$(echo "$cmd_output" | jq -M '.status' | sed 's/"//g')
     percentage=$(echo "$cmd_output" | jq -M '.percentage')
